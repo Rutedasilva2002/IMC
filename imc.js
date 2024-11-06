14392306b9351778ef0259655013cbdf8c7402b0
Rutedasilva2002 <rutedasilvacavalcanti@gmail.com>
2024-11-06 15:12:49 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -3,7 +3,7 @@ function imc(){
     const formDados = new FormData(formulario)
     const paciente = {
         nome:formDados.get("nome"),
-        email:formDados.get("e-mail"),
+        email:formDados.get("email"),
         dataNasc:formDados.get("nasc"),
         telefone:formDados.get("tel"),
         sexo:document.querySelector('input[name=sexo]:checked').value,
@@ -14,12 +14,12 @@ function imc(){
             if (this.altura > 3) this.altura=this.altura/100
             const imc = this.peso/this.altura**2;
             if (imc < 18.5 ) this.classificacao = "magreza";
-            if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudável"
+            if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudÃ¡vel"
             if (imc >= 25 &&  imc < 30 ) this.classificacao = "sobrepeso"
             if (imc >= 30 &&  imc < 35 ) this.classificacao = "obesidade"
             if (imc >= 35 &&  imc < 40 ) this.classificacao = "obesidade Severa"
             if (imc >= 40 ) this.classificacao = "obesidade mórbida"
-            return imc.toFixed(2);
+            return imc.toFixed(4);
         
             
             
@@ -29,5 +29,5 @@ function imc(){
     console.log(paciente)
     console.log(paciente.calcularIMC())
     let resposta=document.getElementById('resposta');
-    resposta.innerHTML="Seu IMC " +paciente.calcularIMC()+". " + "Sua classificação de peso " + paciente.classificacao.;
+    resposta.innerHTML="Seu IMC: "+paciente.calcularIMC()+". " + "Sua classificação de peso " + paciente.classificacao;
 }
